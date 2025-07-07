b888e9a094a07a6a51b30be5ba5009577f45951c
HollowDude <madafakn@gmail.com>
2025-07-06 20:16:51 -0700

    statics

--- app/models/item_carrito.py
+++ app/models/item_carrito.py
@@ -2,6 +2,8 @@ from django.db import models
 from .producto import Producto
 from .carrito import Carrito
 
+'''Al final no use esto pero si gestionase como debe ser los pedidos es necesario'''
+
 class ItemCarrito(models.Model):
     carrito   = models.ForeignKey(Carrito, on_delete=models.CASCADE, related_name='items')
     product   = models.ForeignKey(Producto, on_delete=models.CASCADE, related_name='carrito_items')
