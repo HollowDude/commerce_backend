991f545e3f3067223462f9f8dd4b2966e167d759
HollowDude <madafakn@gmail.com>
2025-07-06 20:20:17 -0700

    statics2

--- commerce/settings.py
+++ commerce/settings.py
@@ -22,7 +22,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
     'rest_framework',
